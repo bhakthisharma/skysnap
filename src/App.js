4b92ed03133893c9f76c72a322f0557eeffc84dd
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-07-06 20:26:25 +0530

    Building weather app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import './App.css';
+function App() {
+  return (
+    <div className="container" style={{backgroundColor:'#0A111E' }}>
+
+      
+      <div className="section-1">
+        <input type="text" name='query' placeholder='Search...' />
+
+      </div>
+      
+      
+
+
+    </div>
+    
+  );
+}
+
+export default App;
