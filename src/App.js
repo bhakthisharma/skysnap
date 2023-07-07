fd37340074370f2fb684bdea1317e480339d77d2
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-07-07 23:42:38 +0530

    Done with the left section design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import {useState} from 'react';
+import searchIcon from './images/search.svg';
+import weatherIcon from './images/weather.png';
+import cloudIcon from './images/cloud.svg';
+import waterIcon from './images/water.svg';
+import sunIcon from './images/sun.svg';
+import locationIcon from './images/location.svg';
+
+
 import './App.css';
 function App() {
   return (
@@ -6,14 +14,54 @@ function App() {
 
       
       <div className="section-1">
+        <div className='top-section'>
+        <div className='search-container'>
         <input type="text" name='query' placeholder='Search...' />
+        <img className='searchimage' src={searchIcon} alt="Search Icon " height={40} width={40} />
+        
+        </div>
+
+      <div className="weatherImage">
+        <img className="weatherimage" src={weatherIcon} alt="Weather Icon " height={40} width={40} />
+        
+        </div>
+        <div className='temp-date'>
+        <h1 className='temp'>30.9<sup className='celcius'>°C</sup></h1>
+        <p className='date'>Friday, 16:12PM</p>
+        </div>
+      </div>
+
+
+      <div className='bottom-section'>
+        <div className='details'>
+
+
+      <div className='cloudimage' >
+      <img  src={cloudIcon} alt="Cloud Icon " height={26} width={26} />
+      <p className='cloud-details'>Partially cloudy</p>
+      </div>
+
+      <div className='waterimage'>
+      <img  src={waterIcon} alt="Water Icon " height={26} width={26} />
+      <p className='water-details'>Misty</p>
+      </div>
+
+      <div className='sunimage'>
+      <img  src={sunIcon} alt="Sun Icon " height={26} width={26} />
+      <p className='sun-details'>21 °C</p>
+      </div>
+
+      
+      
 
       </div>
-      
-      
-
-
-    </div>
+      <div className='locationimage'>
+      <img  src={locationIcon} alt="Location Icon " height={26} width={26} />
+      <p className='location-details'>Manchi,    Kundamkuzhy</p>
+      </div>
+      </div>
+      </div>
+      </div>
     
   );
 }
